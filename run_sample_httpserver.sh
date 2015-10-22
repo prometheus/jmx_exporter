96b755e98cc04f20e0a4682913a158b499951036
Brian Brazil <brian.brazil@robustperception.io>
2015-10-22 11:02:13 +0100

    Update sample script

--- run_sample_httpserver.sh
+++ run_sample_httpserver.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Script to run a java application for testing jmx4prometheus.
 
-java -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=5555 -jar jmx_prometheus_httpserver/target/jmx_prometheus_httpserver-0.5-SNAPSHOT-jar-with-dependencies.jar 5556 sample_config.yml
+java -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=5555 -jar jmx_prometheus_httpserver/target/jmx_prometheus_httpserver-0.6-SNAPSHOT-jar-with-dependencies.jar 5556 sample_config.yml
