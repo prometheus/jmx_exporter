298dcdc14664325e41a432e49d252b815d0b0c2c
Marcel May <marcel.may.de@gmail.com>
2016-11-29 23:32:39 +0100

    Issue 98 : Support configuring host address for WebServer exporter setup (#99)
    * Issue 98 : Support configuring host address for WebServer exporter setup

--- jmx_prometheus_httpserver/src/main/java/io/prometheus/jmx/WebServer.java
+++ jmx_prometheus_httpserver/src/main/java/io/prometheus/jmx/WebServer.java
@@ -1,21 +1,36 @@
 package io.prometheus.jmx;
 
 import io.prometheus.client.exporter.MetricsServlet;
-import java.io.File;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import java.io.File;
+import java.net.InetSocketAddress;
+
 public class WebServer {
+
    public static void main(String[] args) throws Exception {
      if (args.length < 2) {
-       System.err.println("Usage: WebServer <port> <yaml configuration file>");
+       System.err.println("Usage: WebServer <[hostname:]port> <yaml configuration file>");
        System.exit(1);
      }
+
+     String[] hostnamePort = args[0].split(":");
+     int port;
+     InetSocketAddress socket;
+
+     if (hostnamePort.length == 2) {
+       port = Integer.parseInt(hostnamePort[1]);
+       socket = new InetSocketAddress(hostnamePort[0], port);
+     } else {
+       port = Integer.parseInt(hostnamePort[0]);
+       socket = new InetSocketAddress(port);
+     }
+
      JmxCollector jc = new JmxCollector(new File(args[1])).register();
 
-     int port = Integer.parseInt(args[0]);
-     Server server = new Server(port);
+     Server server = new Server(socket);
      ServletContextHandler context = new ServletContextHandler();
      context.setContextPath("/");
      server.setHandler(context);
