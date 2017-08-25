d79071cc489eddc3ccacae9b49d7cc84aa9ccfec
WillCup <willcup@163.com>
2017-08-25 17:49:18 +0800

    suppport multiple instances for one http server

--- jmx_prometheus_httpserver/src/main/java/io/prometheus/jmx/WebServer.java
+++ jmx_prometheus_httpserver/src/main/java/io/prometheus/jmx/WebServer.java
@@ -1,32 +1,34 @@
 package io.prometheus.jmx;
 
-import java.io.File;
-import java.net.InetSocketAddress;
-
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.HTTPServer;
 
+import java.io.File;
+import java.net.InetSocketAddress;
+
 public class WebServer {
 
-   public static void main(String[] args) throws Exception {
-     if (args.length < 2) {
-       System.err.println("Usage: WebServer <[hostname:]port> <yaml configuration file>");
-       System.exit(1);
-     }
+    public static void main(String[] args) throws Exception {
+        if (args.length < 2) {
+            System.err.println("Usage: WebServer <[hostname:]port> <yaml configuration file>");
+            System.exit(1);
+        }
 
-     String[] hostnamePort = args[0].split(":");
-     int port;
-     InetSocketAddress socket;
-     
-     if (hostnamePort.length == 2) {
-       port = Integer.parseInt(hostnamePort[1]);
-       socket = new InetSocketAddress(hostnamePort[0], port);
-     } else {
-       port = Integer.parseInt(hostnamePort[0]);
-       socket = new InetSocketAddress(port);
-     }
+        String[] hostnamePort = args[0].split(":");
+        int port;
+        InetSocketAddress socket;
 
-     new JmxCollector(new File(args[1])).register();
-     new HTTPServer(socket, CollectorRegistry.defaultRegistry);
-   }
+        if (hostnamePort.length == 2) {
+            port = Integer.parseInt(hostnamePort[1]);
+            socket = new InetSocketAddress(hostnamePort[0], port);
+        } else {
+            port = Integer.parseInt(hostnamePort[0]);
+            socket = new InetSocketAddress(port);
+        }
+
+        for (String f : args[1].split(",")){
+            new JmxCollector(new File(f)).register();
+        }
+        new HTTPServer(socket, CollectorRegistry.defaultRegistry);
+    }
 }
