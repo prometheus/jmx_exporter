f28d57661f32e227b2230016ff4dfd25fbf66081
Ivan Babrou <ibobrik@gmail.com>
2018-01-01 09:37:17 -0800

    Support for IPv6 bind (#222)
    This enables bind to IPv6 address in square brackets:
    
    ```
    -javaagent:/usr/share/jmx_exporter/agent.jar=[f00::123]:9097:/etc/kafka/jmx-exporter.yaml
    ```

--- jmx_prometheus_javaagent/src/main/java/io/prometheus/jmx/JavaAgent.java
+++ jmx_prometheus_javaagent/src/main/java/io/prometheus/jmx/JavaAgent.java
@@ -13,6 +13,17 @@ public class JavaAgent {
    static HTTPServer server;
 
    public static void premain(String agentArgument, Instrumentation instrumentation) throws Exception {
+     // Bind to all interfaces by default (this includes IPv6).
+     String host = "0.0.0.0";
+
+     // If we have IPv6 address in square brackets, extract it first and then
+     // remove it from arguments to prevent confusion from too namy colons.
+     Integer indexOfClosingSquareBracket = agentArgument.indexOf("]:");
+     if (indexOfClosingSquareBracket >= 0) {
+       host = agentArgument.substring(0, indexOfClosingSquareBracket + 1);
+       agentArgument = agentArgument.substring(indexOfClosingSquareBracket + 2);
+     }
+
      String[] args = agentArgument.split(":");
      if (args.length < 2 || args.length > 3) {
        System.err.println("Usage: -javaagent:/path/to/JavaAgent.jar=[host:]<port>:<yaml configuration file>");
@@ -29,7 +40,7 @@ public class JavaAgent {
        file = args[2];
      } else {
        port = Integer.parseInt(args[0]);
-       socket = new InetSocketAddress(port);
+       socket = new InetSocketAddress(host, port);
        file = args[1];
      }
 
