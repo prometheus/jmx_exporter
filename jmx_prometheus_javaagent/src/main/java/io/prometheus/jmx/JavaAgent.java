485f68402c25f12fcceb81fe2977a5c8d6d1f57d
mvd <mvdobrinin@gmail.com>
2019-04-09 08:31:11 -0500

    Fix handling of Windows paths in -javaagent config arguments (#312) (#371)
    Signed-off-by: Mikhail Dobrinin <mikhail.dobrinin@jda.com>

--- jmx_prometheus_javaagent/src/main/java/io/prometheus/jmx/JavaAgent.java
+++ jmx_prometheus_javaagent/src/main/java/io/prometheus/jmx/JavaAgent.java
@@ -3,6 +3,8 @@ package io.prometheus.jmx;
 import java.io.File;
 import java.lang.instrument.Instrumentation;
 import java.net.InetSocketAddress;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import io.prometheus.client.CollectorRegistry;
 import io.prometheus.client.exporter.HTTPServer;
@@ -10,47 +12,77 @@ import io.prometheus.client.hotspot.DefaultExports;
 
 public class JavaAgent {
 
-   static HTTPServer server;
+    static HTTPServer server;
 
-   public static void agentmain(String agentArgument, Instrumentation instrumentation) throws Exception {
-     premain(agentArgument, instrumentation);
-   }
+    public static void agentmain(String agentArgument, Instrumentation instrumentation) throws Exception {
+        premain(agentArgument, instrumentation);
+    }
 
-   public static void premain(String agentArgument, Instrumentation instrumentation) throws Exception {
-     // Bind to all interfaces by default (this includes IPv6).
-     String host = "0.0.0.0";
+    public static void premain(String agentArgument, Instrumentation instrumentation) throws Exception {
+        // Bind to all interfaces by default (this includes IPv6).
+        String host = "0.0.0.0";
 
-     // If we have IPv6 address in square brackets, extract it first and then
-     // remove it from arguments to prevent confusion from too many colons.
-     Integer indexOfClosingSquareBracket = agentArgument.indexOf("]:");
-     if (indexOfClosingSquareBracket >= 0) {
-       host = agentArgument.substring(0, indexOfClosingSquareBracket + 1);
-       agentArgument = agentArgument.substring(indexOfClosingSquareBracket + 2);
-     }
+        try {
+            Config config = parseConfig(agentArgument, host);
 
-     String[] args = agentArgument.split(":");
-     if (args.length < 2 || args.length > 3) {
-       System.err.println("Usage: -javaagent:/path/to/JavaAgent.jar=[host:]<port>:<yaml configuration file>");
-       System.exit(1);
-     }
+            new BuildInfoCollector().register();
+            new JmxCollector(new File(config.file)).register();
+            DefaultExports.initialize();
+            server = new HTTPServer(config.socket, CollectorRegistry.defaultRegistry, true);
+        }
+        catch (IllegalArgumentException e) {
+            System.err.println("Usage: -javaagent:/path/to/JavaAgent.jar=[host:]<port>:<yaml configuration file>");
+            System.exit(1);
+        }
+    }
 
-     int port;
-     String file;
-     InetSocketAddress socket;
+    /**
+     * Parse the Java Agent configuration. The arguments are typically specified to the JVM as a javaagent as
+     * {@code -javaagent:/path/to/agent.jar=<CONFIG>}. This method parses the {@code <CONFIG>} portion.
+     * @param args provided agent args
+     * @param ifc default bind interface
+     * @return configuration to use for our application
+     */
+    public static Config parseConfig(String args, String ifc) {
+        Pattern pattern = Pattern.compile(
+                "^(?:((?:[\\w.]+)|(?:\\[.+])):)?" +  // host name, or ipv4, or ipv6 address in brackets
+                        "(\\d{1,5}):" +              // port
+                        "(.+)");                     // config file
 
-     if (args.length == 3) {
-       port = Integer.parseInt(args[1]);
-       socket = new InetSocketAddress(args[0], port);
-       file = args[2];
-     } else {
-       port = Integer.parseInt(args[0]);
-       socket = new InetSocketAddress(host, port);
-       file = args[1];
-     }
+        Matcher matcher = pattern.matcher(args);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("Malformed arguments - " + args);
+        }
 
-     new BuildInfoCollector().register();
-     new JmxCollector(new File(file)).register();
-     DefaultExports.initialize();
-     server = new HTTPServer(socket, CollectorRegistry.defaultRegistry, true);
-   }
+        String givenHost = matcher.group(1);
+        String givenPort = matcher.group(2);
+        String givenConfigFile = matcher.group(3);
+
+        int port = Integer.parseInt(givenPort);
+
+        InetSocketAddress socket;
+        if (givenHost != null && !givenHost.isEmpty()) {
+            socket = new InetSocketAddress(givenHost, port);
+        }
+        else {
+            socket = new InetSocketAddress(ifc, port);
+            givenHost = ifc;
+        }
+
+        return new Config(givenHost, port, givenConfigFile, socket);
+    }
+
+    static class Config {
+        String host;
+        int port;
+        String file;
+        InetSocketAddress socket;
+
+        Config(String host, int port, String file, InetSocketAddress socket) {
+            this.host = host;
+            this.port = port;
+            this.file = file;
+            this.socket = socket;
+        }
+    }
 }
