0e8cb74e72bbd1d05af950a9c1bf7348a0ffdb34
dhoard <doug.hoard@gmail.com>
2023-04-10 08:41:42 -0400

    Fixed formatting
    Signed-off-by: dhoard <doug.hoard@gmail.com>

--- integration_test_suite/jmx_example_application/src/main/java/io/prometheus/jmx/JmxExampleApplication.java
+++ integration_test_suite/jmx_example_application/src/main/java/io/prometheus/jmx/JmxExampleApplication.java
@@ -9,19 +9,19 @@ import java.util.Locale;
 
 public class JmxExampleApplication {
 
-  private static final SimpleDateFormat SIMPLE_DATE_FORMAT =
-          new SimpleDateFormat("yyyy-MM-dd | HH:mm:ss.SSS", Locale.getDefault());
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT =
+        new SimpleDateFormat("yyyy-MM-dd | HH:mm:ss.SSS", Locale.getDefault());
 
-  public static void main(String[] args) throws Exception {
-    MBeanServer server = ManagementFactory.getPlatformMBeanServer();
-    ObjectName tabularMBean = new ObjectName("io.prometheus.jmx:type=tabularData");
-    server.registerMBean(new TabularMBean(), tabularMBean);
-    System.out.println(
-            String.format("%s | %s | INFO | %s | %s",
-                    SIMPLE_DATE_FORMAT.format(new Date()),
-                    Thread.currentThread().getName(),
-                    JmxExampleApplication.class.getName(),
-                    "Running"));
-    Thread.currentThread().join(); // wait forever
-  }
+    public static void main(String[] args) throws Exception {
+        MBeanServer server = ManagementFactory.getPlatformMBeanServer();
+        ObjectName tabularMBean = new ObjectName("io.prometheus.jmx:type=tabularData");
+        server.registerMBean(new TabularMBean(), tabularMBean);
+        System.out.println(
+                String.format("%s | %s | INFO | %s | %s",
+                        SIMPLE_DATE_FORMAT.format(new Date()),
+                        Thread.currentThread().getName(),
+                        JmxExampleApplication.class.getName(),
+                        "Running"));
+        Thread.currentThread().join(); // wait forever
+    }
 }
