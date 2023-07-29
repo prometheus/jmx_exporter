c76c361d71630b16be7e48fb57bcb8fc4150f3df
Doug Hoard <doug.hoard@gmail.com>
2023-07-29 12:04:33 -0400

    Spotless license header changes
    Signed-off-by: Doug Hoard <doug.hoard@gmail.com>

--- collector/src/test/java/io/prometheus/jmx/BuildInfoCollectorTest.java
+++ collector/src/test/java/io/prometheus/jmx/BuildInfoCollectorTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.prometheus.jmx;
 
 import static org.junit.Assert.assertEquals;
--- jmx_prometheus_common/src/test/java/io/prometheus/jmx/common/http/authenticator/PlaintextAuthenticatorTest.java
+++ jmx_prometheus_common/src/test/java/io/prometheus/jmx/common/http/authenticator/PlaintextAuthenticatorTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.prometheus.jmx.common.http.authenticator;
 
 import static org.junit.Assert.assertEquals;
