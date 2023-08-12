8011d0691d57ba571aec4e969be57c715898950f
dhoard <doug.hoard@gmail.com>
2023-08-12 17:33:05 -0400

    Fixed script

--- tools/patch-and-run-integration-test-suite.sh
+++ tools/patch-and-run-integration-test-suite.sh
@@ -9,6 +9,12 @@ function check_exit_code () {
   fi
 }
 
+# Function to emit an error message and exit
+function emit_error () {
+  echo "${1}"
+  exit 1;
+}
+
 JAVA_AGENT_JAR="${1}"
 HTTPSERVER_JAR="${2}"
 PROJECT_ROOT_DIRECTORY=$(git rev-parse --show-toplevel)
@@ -38,8 +44,34 @@ check_exit_code "Failed to patch the integration test suite [${HTTPSERVER_JAR}]"
 ./integration_test_suite/docker-pull-images.smoke-test.sh
 check_exit_code "Failed to update smoke test Docker images"
 
-./mvnw clean verify
+# Change to the integration test suite project
+cd integration_test_suite
+
+# Run the integration test suite
+../mvnw clean verify
 check_exit_code "Integration test suite failed"
+
+# Validate the java agent jar version
+DOWNLOAD_JAVA_AGENT_JAR_SHA256=$(sha256sum "${JAVA_AGENT_JAR}")
+INTEGRATION_TEST_JAVA_AGENT_JAR_SHA256=$(sha256sum ./integration_tests/src/test/resources/common/jmx_prometheus_javaagent.jar)
+
+if [ "${DOWNLOAD_JAVA_AGENT_JAR_SHA256}" != "${DOWNLOAD_JAVA_AGENT_JAR_SHA256}" ];
+then
+  emit_error "Java agent jar mismatch"
+fi
+
+# Validate the java httpserver jar version
+DOWNLOAD_HTTPSERVER_JAR_SHA256=$(sha256sum "${HTTPSERVER_JAR}")
+INTEGRATION_HTTPSERVER_JAR_SHA256=$(sha256sum ./integration_tests/src/test/resources/common/jmx_prometheus_httpserver.jar)
+
+if [ "${DOWNLOAD_HTTPSERVER_JAR_SHA256}" != "${DOWNLOAD_HTTPSERVER_JAR_SHA256}" ];
+then
+  emit_error "Java HTTP server jar mismatch"
+fi
+
+# Change to the project root directory
+cd "${PROJECT_ROOT_DIRECTORY}"
+check_exit_code "Failed to change to project root directory"
 
 echo "------------------------------------------------------------------------"
 echo "SUCCESS"
