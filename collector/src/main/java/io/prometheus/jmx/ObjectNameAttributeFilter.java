d91e13846ef940179a74739c7d452f29a71602fb
kgedminas <kgedminas@eisgroup.com>
2023-12-05 09:26:18 +0200

    Support ObjectName patterns in excludeObjectNameAttributes
    Signed-off-by: kgedminas <kgedminas@eisgroup.com>

--- collector/src/main/java/io/prometheus/jmx/ObjectNameAttributeFilter.java
+++ collector/src/main/java/io/prometheus/jmx/ObjectNameAttributeFilter.java
@@ -129,10 +129,23 @@ public class ObjectNameAttributeFilter {
     public boolean exclude(ObjectName objectName, String attributeName) {
         boolean result = false;
 
+        // concrete objectName definition gets precedence
         if (excludeObjectNameAttributesMap.size() > 0) {
             Set<String> attributeNameSet = excludeObjectNameAttributesMap.get(objectName);
             if (attributeNameSet != null) {
-                result = attributeNameSet.contains(attributeName);
+                return attributeNameSet.contains(attributeName);
+            }
+        }
+        for (Map.Entry<ObjectName, Set<String>> objectNameSetEntry :
+                excludeObjectNameAttributesMap.entrySet()) {
+            if (objectNameSetEntry.getKey().isPattern()
+                    && objectNameSetEntry.getKey().apply(objectName)) {
+                // if exclusion found - return
+                // otherwise keep searching as checked object may match multiple patterns
+                // and checked attribute may be defined only under one of them
+                if (objectNameSetEntry.getValue().contains(attributeName)) {
+                    return true;
+                }
             }
         }
 
