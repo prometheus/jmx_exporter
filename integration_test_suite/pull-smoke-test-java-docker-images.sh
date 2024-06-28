a846e5cf21bebe5983886b1e7e6f0cc6671cb234
dhoard <doug.hoard@gmail.com>
2024-06-28 00:32:56 -0400

    Changed script name
    Signed-off-by: dhoard <doug.hoard@gmail.com>

---
+++ integration_test_suite/pull-smoke-test-java-docker-images.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+PWD="$PWD"
+function exit_trap() {
+  cd "${PWD}"
+  echo $?
+}
+trap exit_trap EXIT
+SCRIPT_DIRECTORY=$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)
+cd "${SCRIPT_DIRECTORY}"
+
+function check_exit_code() {
+  if [ "$?" != "0" ];
+  then
+    echo "Failed to pull Docker image ${1}";
+    exit $?
+  fi
+}
+
+grep -v '^#' integration_tests/src/test/resources/smoke-test-java-docker-images.txt | while read -r LINE;
+do
+  docker pull "${LINE}"
+  check_exit_code "${LINE}"
+done
