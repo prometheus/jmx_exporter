6ef71f637038d263a80c891861a42b62165c7ee4
dhoard <doug.hoard@gmail.com>
2024-07-24 12:58:38 -0400

    Added missing class
    Signed-off-by: dhoard <doug.hoard@gmail.com>

---
+++ jmx_prometheus_common/src/main/java/io/prometheus/jmx/common/util/ResourceSupport.java
@@ -0,0 +1,49 @@
+package io.prometheus.jmx.common.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+/** Class to implement ResourceSupport */
+public class ResourceSupport {
+
+    /** Constructor */
+    private ResourceSupport() {
+        // DO NOTHING
+    }
+
+    public static String load(String resource) throws IOException {
+        Precondition.notNull(resource, "resource is null");
+
+        if (!resource.startsWith("/")) {
+            resource = "/" + resource;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        InputStream inputStream = ResourceSupport.class.getResourceAsStream(resource);
+        if (inputStream == null) {
+            throw new IOException("Resource [" + resource + "] not found");
+        }
+
+        try (BufferedReader bufferedReader =
+                new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            while (true) {
+                String line = bufferedReader.readLine();
+                if (line == null) {
+                    break;
+                }
+
+                if (stringBuilder.length() > 0) {
+                    stringBuilder.append(System.lineSeparator());
+                }
+
+                stringBuilder.append(line);
+            }
+        }
+
+        return stringBuilder.toString();
+    }
+}
