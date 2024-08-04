01bfc9283d209decc65531f4bc0f129314246baa
dhoard <doug.hoard@gmail.com>
2024-08-04 15:56:55 -0400

    Changed/fix shell script
    Signed-off-by: dhoard <doug.hoard@gmail.com>

--- run_sample_standalone.sh
+++ run_sample_standalone.sh
@@ -2,7 +2,7 @@
 
 # Script to run a java application for testing the Prometheus JMX Standalone exporter.
 
-VERSION=$(sed -n -e 's#.*<version>\(.*-SNAPSHOT\)</version>#\1#p' pom.xml)
+VERSION=$(grep "<version>" pom.xml | head -1 | sed -n 's/.*<version>\(.*\)<\/version>.*/\1/p')
 
 # Note: You can use localhost:5556 instead of 5556 for configuring socket hostname.
 
@@ -11,4 +11,4 @@ java \
   -Dcom.sun.management.jmxremote.authenticate=false \
   -Dcom.sun.management.jmxremote.port=5555 \
   -jar jmx_prometheus_standalone/target/jmx_prometheus_standalone-"${VERSION}".jar 5556 \
-  example_configs/httpserver_sample_config.yml
+  example_configs/standalone_sample_config.yml
