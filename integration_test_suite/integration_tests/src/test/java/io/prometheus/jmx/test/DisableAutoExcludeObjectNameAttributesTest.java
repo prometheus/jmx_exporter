81c3b0e807bcfcfa54eacfca9ae8bf9d11eaaa4c
dhoard <doug.hoard@gmail.com>
2024-11-01 00:38:15 -0400

    Updated copyright
    Signed-off-by: dhoard <doug.hoard@gmail.com>

--- integration_test_suite/integration_tests/src/test/java/io/prometheus/jmx/test/DisableAutoExcludeObjectNameAttributesTest.java
+++ integration_test_suite/integration_tests/src/test/java/io/prometheus/jmx/test/DisableAutoExcludeObjectNameAttributesTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 The Prometheus jmx_exporter Authors
+ * Copyright (C) 2023-present The Prometheus jmx_exporter Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
