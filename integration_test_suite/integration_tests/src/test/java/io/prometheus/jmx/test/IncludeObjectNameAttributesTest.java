4a819e706ff902a43217d9162f823b0f27b5081f
dhoard <doug.hoard@gmail.com>
2024-11-22 08:39:01 -0500

    Changed test parallelism
    Signed-off-by: dhoard <doug.hoard@gmail.com>

--- integration_test_suite/integration_tests/src/test/java/io/prometheus/jmx/test/IncludeObjectNameAttributesTest.java
+++ integration_test_suite/integration_tests/src/test/java/io/prometheus/jmx/test/IncludeObjectNameAttributesTest.java
@@ -47,7 +47,7 @@ import org.verifyica.api.Verifyica;
 
 public class IncludeObjectNameAttributesTest {
 
-    @Verifyica.ArgumentSupplier(parallelism = 4)
+    @Verifyica.ArgumentSupplier(parallelism = Integer.MAX_VALUE)
     public static Stream<ExporterTestEnvironment> arguments() {
         return ExporterTestEnvironmentFactory.createExporterTestEnvironments();
     }
