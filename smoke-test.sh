2bb4772fd70e80e11edaba6cd84b9ef5ad7ea233
dhoard <doug.hoard@gmail.com>
2024-11-22 08:37:21 -0500

    Added helper scripts
    Signed-off-by: dhoard <doug.hoard@gmail.com>

---
+++ smoke-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2022-2023 The Prometheus jmx_exporter Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Bash script to build, package, and run the integration
+# test suite continuously until there is a failure
+#
+
+(
+  unset JAVA_DOCKER_IMAGES
+  unset PROMETHEUS_DOCKER_IMAGES
+  ./integration_test_suite/pull-docker-images.sh
+  ./mvnw clean verify
+) 2>&1 | tee smoke-test.log
