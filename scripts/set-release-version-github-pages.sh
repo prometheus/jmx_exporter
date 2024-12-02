f496806f8f5475b9626c3f39e3100ba6c1ca3b64
Doug Hoard <dhoard@users.noreply.github.com>
2024-12-02 11:26:24 -0500

    Delete scripts directory
    Signed-off-by: Doug Hoard <dhoard@users.noreply.github.com>

--- scripts/set-release-version-github-pages.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -euox pipefail
-
-version=$(git tag -l | grep 'v' | sort | tail -1 | sed 's/v//')
-marker="\$version"
-sed -i "s/$marker/$version/g" docs/content/getting-started/quickstart.md
