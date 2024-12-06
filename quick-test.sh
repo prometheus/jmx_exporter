8d2439de42e544b13ff15844827f5dfef5e938f9
dhoard <doug.hoard@gmail.com>
2024-12-06 13:16:32 -0500

    Refactored build to publish collector jar to Maven Central
    Signed-off-by: dhoard <doug.hoard@gmail.com>

---
+++ quick-test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2022-2023 The Prometheus jmx_exporter Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Bash script to build, package, and run the tests (single Docker container)
+#
+
+(
+  export JAVA_DOCKER_IMAGES="eclipse-temurin:21-alpine"
+  export PROMETHEUS_DOCKER_IMAGES="prom/prometheus:v3.0.1"
+  docker pull "$JAVA_DOCKER_IMAGES"
+  docker pull "$PROMETHEUS_DOCKER_IMAGES"
+  ./mvnw clean verify
+) 2>&1 | tee quick-test.log
