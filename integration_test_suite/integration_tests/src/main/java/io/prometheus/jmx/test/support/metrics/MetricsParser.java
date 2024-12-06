b37ed5a10db9e81132b177738a0824a6f64caeb4
dhoard <doug.hoard@gmail.com>
2024-12-06 07:28:17 -0500

    Changed code to throws MetricsParserException if there is not content type in the HttpResponse
    Signed-off-by: dhoard <doug.hoard@gmail.com>

--- integration_test_suite/integration_tests/src/main/java/io/prometheus/jmx/test/support/metrics/MetricsParser.java
+++ integration_test_suite/integration_tests/src/main/java/io/prometheus/jmx/test/support/metrics/MetricsParser.java
@@ -33,7 +33,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
-import java.util.Objects;
 import java.util.TreeMap;
 import java.util.stream.Collectors;
 
@@ -80,7 +79,9 @@ public class MetricsParser {
             return parseProtobufMetrics(httpResponse.body().bytes());
         } else {
             throw new MetricsParserException(
-                    format("Exception parsing text metrics. No parser for CONTENT-TYPE = [%s]", contentType));
+                    format(
+                            "Exception parsing text metrics. No parser for CONTENT-TYPE = [%s]",
+                            contentType));
         }
     }
 
