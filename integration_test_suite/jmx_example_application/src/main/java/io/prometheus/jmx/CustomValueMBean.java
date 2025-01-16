24a323910babd06dd9aa8c5699b9fb348829e3a7
Martin Bickel <martin.bickel@continental.com>
2025-01-16 16:36:48 +0200

    add integration tests for metricCustomizers
    Co-authored-by: Karina Calma <karina_calma@infobest.ro>
    Signed-off-by: Martin Bickel <martin.bickel@continental.com>

---
+++ integration_test_suite/jmx_example_application/src/main/java/io/prometheus/jmx/CustomValueMBean.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2023-present The Prometheus jmx_exporter Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.prometheus.jmx;
+
+/** Class to implement CustomValueMBean */
+public interface CustomValueMBean {
+
+    /**
+     * Method to get the value
+     *
+     * @return value
+     */
+    Integer getValue();
+
+    /**
+     * Method to get the text
+     *
+     * @return text
+     */
+    String getText();
+}
+
+/** Class to implement CustomValue */
+class CustomValue implements CustomValueMBean {
+
+    /** Constructor */
+    public CustomValue() {
+        // INTENTIONALLY BLANK
+    }
+
+    @Override
+    public Integer getValue() {
+        return 345;
+    }
+
+    @Override
+    public String getText() {
+        return "value";
+    }
+}
