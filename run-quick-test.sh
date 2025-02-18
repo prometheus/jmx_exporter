1250cd4bd267a820108b9b4174950a9ac1d901d2
Doug Hoard <dhoard@users.noreply.github.com>
2025-02-18 08:28:59 -0500

    Added Prometheus v3.2.0 to integration tests (#1132)
    Signed-off-by: dhoard <doug.hoard@gmail.com>

--- run-quick-test.sh
+++ run-quick-test.sh
@@ -22,7 +22,7 @@
 
 (
   export JAVA_DOCKER_IMAGES="eclipse-temurin:21-alpine"
-  export PROMETHEUS_DOCKER_IMAGES="prom/prometheus:v3.1.0"
+  export PROMETHEUS_DOCKER_IMAGES="prom/prometheus:v3.2.0"
   docker pull "$JAVA_DOCKER_IMAGES"
   docker pull "$PROMETHEUS_DOCKER_IMAGES"
   ./mvnw clean verify
