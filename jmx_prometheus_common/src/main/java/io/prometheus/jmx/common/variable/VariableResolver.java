740f018785435f6144848a36a54fafad6bba04b4
Doug Hoard <dhoard@users.noreply.github.com>
2025-08-25 12:23:53 -0400

    Refactored environment variable configuration usage (#1297)
    Signed-off-by: dhoard <doug.hoard@gmail.com>

--- jmx_prometheus_common/src/main/java/io/prometheus/jmx/common/variable/VariableResolver.java
+++ jmx_prometheus_common/src/main/java/io/prometheus/jmx/common/variable/VariableResolver.java
@@ -16,28 +16,8 @@
 
 package io.prometheus.jmx.common.variable;
 
-import static java.lang.String.format;
-
-import io.prometheus.jmx.common.variable.provider.EnvironmentVariableProvider;
-import io.prometheus.jmx.common.variable.provider.FileVariableProvider;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-/**
- * Class to resolves variables from multiple sources such as an environment variable, file, or
- * literal.
- *
- * <p>Uses pluggable {@link VariableProvider} instances.
- */
+/** Class to resolves variables. */
 public final class VariableResolver {
-
-    private static final List<VariableProvider> VARIABLE_PROVIDERS = new ArrayList<>();
-
-    static {
-        VARIABLE_PROVIDERS.add(new FileVariableProvider());
-        VARIABLE_PROVIDERS.add(new EnvironmentVariableProvider());
-    }
 
     /** Constructor */
     private VariableResolver() {
@@ -45,74 +25,66 @@ public final class VariableResolver {
     }
 
     /**
-     * Resolves a variable string, which may be...
-     *
-     * <p>${env:MY_ENV_VAR}
-     *
-     * <p>${file:/path/to/secret.txt}
-     *
-     * <p>literal-value
+     * Resolves a variable.
      *
      * @param variable the variable
      * @return the resolved variable value
      */
-    public static String resolve(String variable) {
+    public static String resolveVariable(String variable) {
         // If the variable is null or empty, return the variable
         if (variable == null || variable.trim().isEmpty()) {
             return variable;
         }
 
-        // Get the specification from the variable
-        String variableSpec = getVariableSpec(variable);
-
-        // Loop through all registered variable providers
-        for (VariableProvider variableProvider : VARIABLE_PROVIDERS) {
-            // Check if the provider supports the spec
-            if (variableProvider.supports(variableSpec)) {
-                // Resolve the variable using the provider
-                Optional<String> result = variableProvider.resolve(variableSpec);
-
-                // If the provider successfully resolved the variable, return the variable
-                if (result.isPresent()) {
-                    return result.get();
-                }
-            }
-        }
-
-        // If no provider could resolve the variable, return the original value
-        return variable;
-    }
-
-    /**
-     * Method to extract the string from the variable specification.
-     *
-     * @param variable the variable specification
-     * @return the spec
-     */
-    private static String getVariableSpec(String variable) {
         // Trim the variable to remove leading and trailing whitespace
         String trimmedVariable = variable.trim();
 
         // If exactly "${}", return it as a literal
         if ("${}".equals(trimmedVariable)) {
-            return trimmedVariable;
+            return variable;
         }
 
-        String variableSpec;
-
-        // Handle expressions like ${env:FOO}, ${file:/path/to/secret.txt}, etc.
+        // Handle expressions like ${FOO}
         if (trimmedVariable.startsWith("${") && trimmedVariable.endsWith("}")) {
-            variableSpec = trimmedVariable.substring(2, trimmedVariable.length() - 1).trim();
-
-            // If the spec is empty after trimming, throw an exception
-            if (variableSpec.isEmpty()) {
-                throw new VariableResolverException(
-                        format("Invalid variable format [%s]", variable));
-            }
-        } else {
-            variableSpec = trimmedVariable;
+            return resolveEnvironmentVariable(trimmedVariable);
         }
 
-        return variableSpec;
+        return variable;
+    }
+
+    /**
+     * Resolves an environment variable.
+     *
+     * @param variable the variable
+     * @return the resolved variable value, or null if it can't be resolved
+     */
+    private static String resolveEnvironmentVariable(String variable) {
+        // Remove the ${ and } from the variable
+        String environmentVariableName = variable.substring(2, variable.length() - 1).trim();
+
+        // If the environment variable name is empty after trimming, return null
+        if (environmentVariableName.isEmpty()) {
+            throw new VariableResolverException(
+                    "Invalid environment variable name '" + variable + "'");
+        }
+
+        // Retrieve the value of the environment variable
+        String value = System.getenv(environmentVariableName);
+
+        // If the value is null, throw an exception
+        if (value == null) {
+            throw new VariableResolverException(
+                    "Environment variable '" + variable + "' not defined");
+        }
+
+        // Trim the value to remove leading and trailing whitespace
+        value = value.trim();
+
+        // If the value is empty or only whitespace, throw an exception
+        if (value.isEmpty()) {
+            throw new VariableResolverException("Environment variable '" + variable + "' is empty");
+        }
+
+        return value;
     }
 }
