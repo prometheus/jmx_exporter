91260d7d8220f0c49e8244d841607513948e3af5
dhoard <doug.hoard@gmail.com>
2025-09-17 12:47:49 -0400

    Applied Spotless formatting
    Signed-off-by: dhoard <doug.hoard@gmail.com>

--- collector/src/main/java/io/prometheus/jmx/logger/LoggerFactory.java
+++ collector/src/main/java/io/prometheus/jmx/logger/LoggerFactory.java
@@ -33,8 +33,8 @@ public class LoggerFactory {
     static {
         // Override the default formatter for the root logger if it is SimpleFormatter
         for (Handler handler : java.util.logging.Logger.getLogger(ROOT_LOGGER).getHandlers()) {
-            if (null != handler.getFormatter() &&
-                    handler.getFormatter()
+            if (null != handler.getFormatter()
+                    && handler.getFormatter()
                             .getClass()
                             .getName()
                             .endsWith(SimpleFormatter.class.getName())) {
