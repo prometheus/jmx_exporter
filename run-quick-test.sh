2887b1160eeb5f3086e1509385b4c54e3e0e6134
Doug Hoard <dhoard@users.noreply.github.com>
2025-10-01 10:52:17 -0400

    Update Java Docker image version in script (#1326)
    Signed-off-by: Doug Hoard <dhoard@users.noreply.github.com>

--- run-quick-test.sh
+++ run-quick-test.sh
@@ -21,7 +21,7 @@
 #
 
 (
-  export JAVA_DOCKER_IMAGES="eclipse-temurin:21-alpine"
+  export JAVA_DOCKER_IMAGES="eclipse-temurin:25-alpine"
   export PROMETHEUS_DOCKER_IMAGES="prom/prometheus:v3.5.0"
   docker pull "$JAVA_DOCKER_IMAGES"
   docker pull "$PROMETHEUS_DOCKER_IMAGES"
