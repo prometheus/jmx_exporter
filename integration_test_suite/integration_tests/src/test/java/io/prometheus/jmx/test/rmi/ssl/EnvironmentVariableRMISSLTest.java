fb3415ae12f23aab216376873bcf4759c275832b
dhoard <doug.hoard@gmail.com>
2025-10-06 22:27:35 -0400

    Added integration test using standalone exporter and environment variables
    Signed-off-by: dhoard <doug.hoard@gmail.com>

---
+++ integration_test_suite/integration_tests/src/test/java/io/prometheus/jmx/test/rmi/ssl/EnvironmentVariableRMISSLTest.java
@@ -0,0 +1,228 @@
+/*
+ * Copyright (C) The Prometheus jmx_exporter Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.prometheus.jmx.test.rmi.ssl;
+
+import static io.prometheus.jmx.test.support.http.HttpResponse.assertHealthyResponse;
+import static io.prometheus.jmx.test.support.metrics.MetricAssertion.assertMetric;
+import static io.prometheus.jmx.test.support.metrics.MetricAssertion.assertMetricsContentType;
+
+import io.prometheus.jmx.test.support.environment.JmxExporterMode;
+import io.prometheus.jmx.test.support.environment.JmxExporterPath;
+import io.prometheus.jmx.test.support.environment.JmxExporterTestEnvironment;
+import io.prometheus.jmx.test.support.http.HttpClient;
+import io.prometheus.jmx.test.support.http.HttpHeader;
+import io.prometheus.jmx.test.support.http.HttpResponse;
+import io.prometheus.jmx.test.support.metrics.Metric;
+import io.prometheus.jmx.test.support.metrics.MetricsContentType;
+import io.prometheus.jmx.test.support.metrics.MetricsParser;
+import io.prometheus.jmx.test.support.util.TestSupport;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+import org.testcontainers.containers.Network;
+import org.verifyica.api.ArgumentContext;
+import org.verifyica.api.Trap;
+import org.verifyica.api.Verifyica;
+
+public class EnvironmentVariableRMISSLTest {
+
+    @Verifyica.ArgumentSupplier(parallelism = Integer.MAX_VALUE)
+    public static Stream<JmxExporterTestEnvironment> arguments() {
+        // Filter the arguments..
+        //
+        // 1. only run the Standalone exporter
+        // 2. filter out the GraalVM 1.8 JVM - exception is that SunJSSE is not found
+        // 3. filter out all ibmjava* JVMs - exception is that SunJSSE is not found
+        //
+        return JmxExporterTestEnvironment.createEnvironments()
+                .filter(
+                        exporterTestEnvironment ->
+                                exporterTestEnvironment.getJmxExporterMode()
+                                        == JmxExporterMode.Standalone)
+                .filter(
+                        exporterTestEnvironment ->
+                                !exporterTestEnvironment
+                                        .getJavaDockerImage()
+                                        .contains("graalvm/jdk:java8"))
+                .filter(
+                        exporterTestEnvironment ->
+                                !exporterTestEnvironment.getJavaDockerImage().contains("ibmjava"));
+    }
+
+    @Verifyica.BeforeAll
+    public void beforeAll(ArgumentContext argumentContext) {
+        Class<?> testClass = argumentContext.classContext().testClass();
+        Network network = TestSupport.getOrCreateNetwork(argumentContext);
+        TestSupport.initializeExporterTestEnvironment(argumentContext, network, testClass);
+    }
+
+    @Verifyica.Test
+    @Verifyica.Order(1)
+    public void testHealthy(JmxExporterTestEnvironment jmxExporterTestEnvironment)
+            throws IOException {
+        String url = jmxExporterTestEnvironment.getUrl(JmxExporterPath.HEALTHY);
+
+        HttpResponse httpResponse = HttpClient.sendRequest(url);
+
+        assertHealthyResponse(httpResponse);
+    }
+
+    @Verifyica.Test
+    public void testDefaultTextMetrics(JmxExporterTestEnvironment jmxExporterTestEnvironment)
+            throws IOException {
+        String url = jmxExporterTestEnvironment.getUrl(JmxExporterPath.METRICS);
+
+        HttpResponse httpResponse = HttpClient.sendRequest(url);
+
+        assertMetricsResponse(jmxExporterTestEnvironment, httpResponse, MetricsContentType.DEFAULT);
+    }
+
+    @Verifyica.Test
+    public void testOpenMetricsTextMetrics(JmxExporterTestEnvironment jmxExporterTestEnvironment)
+            throws IOException {
+        String url = jmxExporterTestEnvironment.getUrl(JmxExporterPath.METRICS);
+
+        HttpResponse httpResponse =
+                HttpClient.sendRequest(
+                        url,
+                        HttpHeader.ACCEPT,
+                        MetricsContentType.OPEN_METRICS_TEXT_METRICS.toString());
+
+        assertMetricsResponse(
+                jmxExporterTestEnvironment,
+                httpResponse,
+                MetricsContentType.OPEN_METRICS_TEXT_METRICS);
+    }
+
+    @Verifyica.Test
+    public void testPrometheusTextMetrics(JmxExporterTestEnvironment jmxExporterTestEnvironment)
+            throws IOException {
+        String url = jmxExporterTestEnvironment.getUrl(JmxExporterPath.METRICS);
+
+        HttpResponse httpResponse =
+                HttpClient.sendRequest(
+                        url,
+                        HttpHeader.ACCEPT,
+                        MetricsContentType.PROMETHEUS_TEXT_METRICS.toString());
+
+        assertMetricsResponse(
+                jmxExporterTestEnvironment,
+                httpResponse,
+                MetricsContentType.PROMETHEUS_TEXT_METRICS);
+    }
+
+    @Verifyica.Test
+    public void testPrometheusProtobufMetrics(JmxExporterTestEnvironment jmxExporterTestEnvironment)
+            throws IOException {
+        String url = jmxExporterTestEnvironment.getUrl(JmxExporterPath.METRICS);
+
+        HttpResponse httpResponse =
+                HttpClient.sendRequest(
+                        url,
+                        HttpHeader.ACCEPT,
+                        MetricsContentType.PROMETHEUS_PROTOBUF_METRICS.toString());
+
+        assertMetricsResponse(
+                jmxExporterTestEnvironment,
+                httpResponse,
+                MetricsContentType.PROMETHEUS_PROTOBUF_METRICS);
+    }
+
+    @Verifyica.AfterAll
+    public void afterAll(ArgumentContext argumentContext) throws Throwable {
+        List<Trap> traps = new ArrayList<>();
+
+        traps.add(new Trap(() -> TestSupport.destroyExporterTestEnvironment(argumentContext)));
+        traps.add(new Trap(() -> TestSupport.destroyNetwork(argumentContext)));
+
+        Trap.assertEmpty(traps);
+    }
+
+    private void assertMetricsResponse(
+            JmxExporterTestEnvironment jmxExporterTestEnvironment,
+            HttpResponse httpResponse,
+            MetricsContentType metricsContentType) {
+        assertMetricsContentType(httpResponse, metricsContentType);
+
+        Collection<Metric> metrics = MetricsParser.parseCollection(httpResponse);
+
+        boolean isJmxExporterModeJavaAgent =
+                jmxExporterTestEnvironment.getJmxExporterMode() == JmxExporterMode.JavaAgent;
+
+        String buildInfoName =
+                TestSupport.getBuildInfoName(jmxExporterTestEnvironment.getJmxExporterMode());
+
+        assertMetric(metrics)
+                .ofType(Metric.Type.GAUGE)
+                .withName("jmx_exporter_build_info")
+                .withLabel("name", buildInfoName)
+                .withValue(1d)
+                .isPresent();
+
+        assertMetric(metrics)
+                .ofType(Metric.Type.GAUGE)
+                .withName("jmx_scrape_error")
+                .withValue(0d)
+                .isPresent();
+
+        assertMetric(metrics)
+                .ofType(Metric.Type.COUNTER)
+                .withName("jmx_config_reload_success_total")
+                .withValue(0d)
+                .isPresent();
+
+        assertMetric(metrics)
+                .ofType(Metric.Type.GAUGE)
+                .withName("jvm_memory_used_bytes")
+                .withLabel("area", "nonheap")
+                .isPresentWhen(isJmxExporterModeJavaAgent);
+
+        assertMetric(metrics)
+                .ofType(Metric.Type.GAUGE)
+                .withName("jvm_memory_used_bytes")
+                .withLabel("area", "heap")
+                .isPresentWhen(isJmxExporterModeJavaAgent);
+
+        assertMetric(metrics)
+                .ofType(Metric.Type.GAUGE)
+                .withName("jvm_memory_used_bytes")
+                .withLabel("area", "nonheap")
+                .isPresentWhen(isJmxExporterModeJavaAgent);
+
+        assertMetric(metrics)
+                .ofType(Metric.Type.GAUGE)
+                .withName("jvm_memory_used_bytes")
+                .withLabel("area", "heap")
+                .isPresentWhen(isJmxExporterModeJavaAgent);
+
+        assertMetric(metrics)
+                .ofType(Metric.Type.UNTYPED)
+                .withName("io_prometheus_jmx_tabularData_Server_1_Disk_Usage_Table_size")
+                .withLabel("source", "/dev/sda1")
+                .withValue(7.516192768E9d)
+                .isPresent();
+
+        assertMetric(metrics)
+                .ofType(Metric.Type.UNTYPED)
+                .withName("io_prometheus_jmx_tabularData_Server_2_Disk_Usage_Table_pcent")
+                .withLabel("source", "/dev/sda2")
+                .withValue(0.8d)
+                .isPresent();
+    }
+}
---
+++ integration_test_suite/integration_tests/src/test/resources/io/prometheus/jmx/test/rmi/ssl/EnvironmentVariableRMISSLTest/Standalone/application.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#
+# Code to run the test with RedHat UBI images
+#
+# When running on RedHat UBI images, testcontainers maps
+# the files as the current user, but the application runs
+# as "jboss" on UBI8 images and "default" on UBI9 images
+# preventing the chmod commands to change permissions on
+# the jmxremote.access and jmxremote.password files.
+#
+# The code copies the files to /tmp as the current user
+# then performs a chmod to change permissions.
+#
+
+JMXREMOTE_ACCESS=jmxremote.access
+JMXREMOTE_PASSWORD=jmxremote.password
+
+WHOAMI=$(whoami)
+if [ "${WHOAMI}" = "jboss" ] || [ "${WHOAMI}" = "default" ];
+then
+  cp ${JMXREMOTE_ACCESS} /tmp/jmxremote.access
+  cp ${JMXREMOTE_PASSWORD} /tmp/jmxremote.password
+  chmod go-rwx /tmp/jmxremote.access
+  chmod go-rwx /tmp/jmxremote.password
+  JMXREMOTE_ACCESS=/tmp/jmxremote.access
+  JMXREMOTE_PASSWORD=/tmp/jmxremote.password
+else
+  chmod go-rwx jmxremote.access
+  chmod go-rwx jmxremote.password
+fi
+
+java \
+  -Xmx512M \
+  -Dcom.sun.management.jmxremote=true \
+  -Dcom.sun.management.jmxremote.authenticate=true \
+  -Dcom.sun.management.jmxremote.password.file=${JMXREMOTE_PASSWORD} \
+  -Dcom.sun.management.jmxremote.port=9999 \
+  -Dcom.sun.management.jmxremote.registry.ssl=true \
+  -Dcom.sun.management.jmxremote.access.file=${JMXREMOTE_ACCESS} \
+  -Dcom.sun.management.jmxremote.rmi.port=9999 \
+  -Dcom.sun.management.jmxremote.ssl=true \
+  -Djavax.net.ssl.keyStore=localhost.pkcs12 \
+  -Djavax.net.ssl.keyStorePassword=changeit \
+  -Djavax.net.ssl.keyStoreType=pkcs12 \
+  -Djavax.net.ssl.trustStore=localhost.pkcs12 \
+  -Djavax.net.ssl.trustStorePassword=changeit \
+  -Djavax.net.ssl.trustStoreType=pkcs12 \
+  -jar jmx_example_application.jar
