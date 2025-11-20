1cf92eac6acfb41fcd09987210584dec4c7d7408
Hakky54 <hakangoudberg@hotmail.com>
2025-11-20 16:35:59 +0100

    Removed ssl properties from exporter.sh
    Signed-off-by: Hakky54 <hakangoudberg@hotmail.com>

--- integration_test_suite/integration_tests/src/test/resources/io/prometheus/jmx/test/rmi/ssl/RMISSLFromYamlTest/Standalone/exporter.sh
+++ integration_test_suite/integration_tests/src/test/resources/io/prometheus/jmx/test/rmi/ssl/RMISSLFromYamlTest/Standalone/exporter.sh
@@ -5,10 +5,4 @@ export PASSWORD=secret
 
 java \
   -Xmx512M \
-  -Djavax.net.ssl.keyStore=localhost.pkcs12 \
-  -Djavax.net.ssl.keyStorePassword=changeit \
-  -Djavax.net.ssl.keyStoreType=pkcs12 \
-  -Djavax.net.ssl.trustStore=localhost.pkcs12 \
-  -Djavax.net.ssl.trustStorePassword=changeit \
-  -Djavax.net.ssl.trustStoreType=pkcs12 \
   -jar jmx_prometheus_standalone.jar 8888 exporter.yaml
