2f93cfe1f6ca76b9afcb792fdfd63aad0af6e581
Doug Hoard <dhoard@users.noreply.github.com>
2026-08-26 23:27:25 -0400

    fix: Fixed scripts (#1563)
    Signed-off-by: dhoard <doug.hoard@gmail.com>

--- run-smoke-test.sh
+++ run-smoke-test.sh
@@ -26,7 +26,7 @@ set -o pipefail
 usage() {
   echo "Usage: $0 [parallelism] [-D<name>[=<value>] ...]"
   echo "Quote -D values containing shell metacharacters, for example:"
-  echo "  $0 [parallelism] '-Dparamixel.match.class=^(?!.*PBKDF).*'"
+  echo "  $0 [parallelism] '-Dparamixel.match.class.regex=^(?!.*PBKDF).*'"
 }
 
 CPU_COUNT="$(nproc)"
